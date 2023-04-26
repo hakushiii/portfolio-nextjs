43e20d42c6c33f62ec10ae0eac0c779a60466e7b
Jerome A <jeromealapad13@gmail.com>
2023-04-26 22:35:45 +0800

    fixed weird border

--- pages/tools.tsx
+++ pages/tools.tsx
@@ -8,106 +8,104 @@ const Tools = () => {
         <Head>
             <title>HKSH - Tools</title>
         </Head>
-        <Container>
-            <br /><br /><br />
-            <Heading variant='tools-title'>
-                TOOLS THAT I USE
-            </Heading>
-            <Heading variant='tools-section'>
-                Devices
-            </Heading>
-            <Flex justifyContent='center'>
-                <Box my={1} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>  
-                    <Section>
-                        <Image borderRadius='lg' 
-                               alt='mba m2 midnight' 
-                               src='https://cdn.mos.cms.futurecdn.net/wt28Bmfr6mdPyoKdZwCkWf.jpg'
-                               />
-                        <Heading variant='tools-item'>
-                            <a href='https://www.apple.com/ph/macbook-air-m2/'>Macbook Air M2 [Midnight]</a>
-                        </Heading>
-                    </Section>
-                </Box>
-            </Flex>
-            <Heading variant='tools-section'>
-                Peripherals
-            </Heading>
-            <Flex justifyContent='center'>
-                <Box mr={3} my={2} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>  
-                    <Section>
-                        <Image maxHeight={400} borderRadius='lg' 
-                               alt='keychron k3 v2' 
-                               src='https://cdn.shopify.com/s/files/1/0059/0630/1017/products/Keychron-K3-ultra-slim-wireless-mechanical-keyboard--3_1800x1800.jpg?v=1634644047'
-                               />
-                        <Heading variant='tools-item'>
-                            <a href='https://www.keychron.com/products/keychron-k3-wireless-mechanical-keyboard'>Keychron K3 V2</a>
-                        </Heading>
-                    </Section>
-                </Box>
-                <Box ml={3} my={2} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>  
-                    <Section>
-                        <Image maxHeight={400} borderRadius='lg' 
-                               alt='razer viper pro ult' 
-                               src='https://assets3.razerzone.com/6v1RVaYM7gImsVUwGP7AXb6YOcw=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh78%2Fh6a%2F9081447088158%2Frazer-viper-ultimate-gallery-hero.jpg'
-                               />
-                        <Heading variant='tools-item'>
-                            <a href='https://www.razer.com/gaming-mice/razer-viper-ultimate'>Razer Viper Pro Ultimate</a>
-                        </Heading>
-                    </Section>
-                </Box>
-            </Flex>
-            <Flex justifyContent='center'>
-                <Box mr={3} my={2} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>  
-                    <Section>
-                        <Image maxHeight={400} borderRadius='lg' 
-                               alt='8bitdo' 
-                               src='https://images.idgesg.net/images/article/2021/04/dsc07195_final-100886698-large.jpg'
-                               />
-                        <Heading variant='tools-item'>
-                            <a href='https://www.8bitdo.com/pro2/'>8Bitdo Pro 2 Controller</a>
-                        </Heading>
-                    </Section>
-                </Box>
-                <Box ml={3} my={2} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>  
-                    <Section>
-                        <Image maxHeight={400} borderRadius='lg'
-                               alt='katos' 
-                               src='https://images.mmorpg.com/images/heroes/posts/123565.jpg?cb=2E40083E9AACB09D4C80E1FD5AA5E6C0'
-                               />
-                        <Heading variant='tools-item'>
-                            <a href='https://moondroplab.com/en/products/kato'>MoonDrop Kato</a>
-                        </Heading>
-                    </Section>
-                </Box>
-            </Flex>
-            <Heading variant='tools-section'>
-                Accessories
-            </Heading>
-            <Flex justifyContent='center'>
-                <Box mr={3} my={2} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>  
-                    <Section>
-                        <Image maxHeight={400} borderRadius='lg' 
-                               alt='UGREEN Hub' 
-                               src='https://cdn.shopify.com/s/files/1/0257/5246/9566/files/5640c170-182e-4eb8-9f84-172a95fa49e3.__CR0_0_970_600_PT0_SX970_V1.jpg?v=1658296590'
-                               />
-                        <Heading variant='tools-item'>
-                            <a href='https://www.ugreen.com/collections/usb-hub/products/6-in-1-usb-c-adapter-with-4k-hdmi?variant=39915659722814'>UGREEN 6-in-1 USB Hub</a>
-                        </Heading>
-                    </Section>
-                </Box>
-                <Box ml={3} my={2} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>  
-                    <Section>
-                        <Image maxHeight={400} borderRadius='lg' 
-                               alt='yes, dont ask' 
-                               src='https://m.media-amazon.com/images/I/41agkgDaUJL._AC_UF894,1000_QL80_.jpg'
-                               />
-                        <Heading variant='tools-item'>
-                            <a href='https://www.amazon.ca/Wrong-Pick-Girls-Dungeon-Play/dp/B01LWMUULT'>Danmachi Playmat</a>
-                        </Heading>
-                    </Section>
-                </Box>
-            </Flex>
-        </Container>
+        <br /><br /><br />
+        <Heading variant='tools-title'>
+            TOOLS THAT I USE
+        </Heading>
+        <Heading variant='tools-section'>
+            Devices
+        </Heading>
+        <Flex justifyContent='center'>
+            <Box my={1} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>  
+                <Section>
+                    <Image borderRadius='lg' 
+                           alt='mba m2 midnight' 
+                           src='https://cdn.mos.cms.futurecdn.net/wt28Bmfr6mdPyoKdZwCkWf.jpg'
+                           />
+                    <Heading variant='tools-item'>
+                        <a href='https://www.apple.com/ph/macbook-air-m2/'>Macbook Air M2 [Midnight]</a>
+                    </Heading>
+                </Section>
+            </Box>
+        </Flex>
+        <Heading variant='tools-section'>
+            Peripherals
+        </Heading>
+        <Flex justifyContent='center'>
+            <Box mr={3} my={2} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>  
+                <Section>
+                    <Image maxHeight={400} borderRadius='lg' 
+                           alt='keychron k3 v2' 
+                           src='https://cdn.shopify.com/s/files/1/0059/0630/1017/products/Keychron-K3-ultra-slim-wireless-mechanical-keyboard--3_1800x1800.jpg?v=1634644047'
+                           />
+                    <Heading variant='tools-item'>
+                        <a href='https://www.keychron.com/products/keychron-k3-wireless-mechanical-keyboard'>Keychron K3 V2</a>
+                    </Heading>
+                </Section>
+            </Box>
+            <Box ml={3} my={2} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>  
+                <Section>
+                    <Image maxHeight={400} borderRadius='lg' 
+                           alt='razer viper pro ult' 
+                           src='https://assets3.razerzone.com/6v1RVaYM7gImsVUwGP7AXb6YOcw=/1500x1000/https%3A%2F%2Fhybrismediaprod.blob.core.windows.net%2Fsys-master-phoenix-images-container%2Fh78%2Fh6a%2F9081447088158%2Frazer-viper-ultimate-gallery-hero.jpg'
+                           />
+                    <Heading variant='tools-item'>
+                        <a href='https://www.razer.com/gaming-mice/razer-viper-ultimate'>Razer Viper Pro Ultimate</a>
+                    </Heading>
+                </Section>
+            </Box>
+        </Flex>
+        <Flex justifyContent='center'>
+            <Box mr={3} my={2} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>  
+                <Section>
+                    <Image maxHeight={400} borderRadius='lg' 
+                           alt='8bitdo' 
+                           src='https://images.idgesg.net/images/article/2021/04/dsc07195_final-100886698-large.jpg'
+                           />
+                    <Heading variant='tools-item'>
+                        <a href='https://www.8bitdo.com/pro2/'>8Bitdo Pro 2 Controller</a>
+                    </Heading>
+                </Section>
+            </Box>
+            <Box ml={3} my={2} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>  
+                <Section>
+                    <Image maxHeight={400} borderRadius='lg'
+                           alt='katos' 
+                           src='https://images.mmorpg.com/images/heroes/posts/123565.jpg?cb=2E40083E9AACB09D4C80E1FD5AA5E6C0'
+                           />
+                    <Heading variant='tools-item'>
+                        <a href='https://moondroplab.com/en/products/kato'>MoonDrop Kato</a>
+                    </Heading>
+                </Section>
+            </Box>
+        </Flex>
+        <Heading variant='tools-section'>
+            Accessories
+        </Heading>
+        <Flex justifyContent='center'>
+            <Box mr={3} my={2} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>  
+                <Section>
+                    <Image maxHeight={400} borderRadius='lg' 
+                           alt='UGREEN Hub' 
+                           src='https://cdn.shopify.com/s/files/1/0257/5246/9566/files/5640c170-182e-4eb8-9f84-172a95fa49e3.__CR0_0_970_600_PT0_SX970_V1.jpg?v=1658296590'
+                           />
+                    <Heading variant='tools-item'>
+                        <a href='https://www.ugreen.com/collections/usb-hub/products/6-in-1-usb-c-adapter-with-4k-hdmi?variant=39915659722814'>UGREEN 6-in-1 USB Hub</a>
+                    </Heading>
+                </Section>
+            </Box>
+            <Box ml={3} my={2} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>  
+                <Section>
+                    <Image maxHeight={400} borderRadius='lg' 
+                           alt='yes, dont ask' 
+                           src='https://m.media-amazon.com/images/I/41agkgDaUJL._AC_UF894,1000_QL80_.jpg'
+                           />
+                    <Heading variant='tools-item'>
+                        <a href='https://www.amazon.ca/Wrong-Pick-Girls-Dungeon-Play/dp/B01LWMUULT'>Danmachi Playmat</a>
+                    </Heading>
+                </Section>
+            </Box>
+        </Flex>
     </Container>
     )
 }
--- pages/works.tsx
+++ pages/works.tsx
@@ -7,48 +7,44 @@ const Works = () => {
         <Head>
             <title>HKSH - Works</title>
         </Head>
-        <Box>
-            <br /><br /><br />
-            <Heading variant='works-title'>
-                    WORKS
-            </Heading>
-            <Heading variant='works-item'>
-                &lt; L I B R A R Y &gt; EBOOK-REPOSITORY
-            </Heading>
-            <Flex justifyContent='center'>
-                <Box my={1} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')} p={2}>
-                    <Image borderRadius='lg' 
-                           alt='library repo'
-                           src='/images/library.png'
-                    />
-                     <Grid templateColumns='repeat(2, 1fr)' px={100}>
-                      <GridItem>
-                        <Box mx={10} p={2} textAlign='center'>
-                            <Text variant='work-lang'>LANG used</Text>
-                            <List>
-                                <ListItem><a href='https://python.org'>Python</a></ListItem>
-                                <ListItem><a href='https://javascript.com'>Javascript</a></ListItem>
-                            </List>
-                        </Box>    
-                      </GridItem>
-                      <GridItem>
-                        <Box mx={10} p={2} textAlign='center'>
-                            <Text variant='work-stack'>TECH used</Text>
-                            <List>
-                                <ListItem><a href='https://react.dev'>React</a></ListItem>
-                                <ListItem><a href='https://nodejs.org'>Node.js</a></ListItem>
-                                <ListItem><a href='https://fastapi.tiangolo.com'>FastAPI</a></ListItem>
-                                <ListItem><a href='https://mongodb.com'>MongoDB</a></ListItem>
-                                <ListItem><a href='https://railway.org'>Railway</a></ListItem>
-                            </List>
-                        </Box>
-                      </GridItem>
-                    </Grid>
-                    <Flex alignSelf='center'>
-                    </Flex>
-                </Box>    
-            </Flex>
-        </Box>
+        <br /><br /><br />
+        <Heading variant='works-title'>
+                WORKS
+        </Heading>
+        <Heading variant='works-item'>
+            &lt; L I B R A R Y &gt; EBOOK-REPOSITORY
+        </Heading>
+        <Flex justifyContent='center'>
+            <Box my={1} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>
+                <Image borderRadius='lg'
+                       alt='library repo'
+                       src='/images/library.png'
+                        />
+                 <Grid templateColumns='repeat(2, 1fr)'>
+                  <GridItem>
+                    <Box mx={10} p={2} textAlign='center'>
+                        <Text variant='work-lang'>LANG used</Text>
+                        <List>
+                            <ListItem><a href='https://python.org'>Python</a></ListItem>
+                            <ListItem><a href='https://javascript.com'>Javascript</a></ListItem>
+                        </List>
+                    </Box>    
+                  </GridItem>
+                  <GridItem>
+                    <Box mx={10} p={2} textAlign='center'>
+                        <Text variant='work-stack'>TECH used</Text>
+                        <List>
+                            <ListItem><a href='https://react.dev'>React</a></ListItem>
+                            <ListItem><a href='https://nodejs.org'>Node.js</a></ListItem>
+                            <ListItem><a href='https://fastapi.tiangolo.com'>FastAPI</a></ListItem>
+                            <ListItem><a href='https://mongodb.com'>MongoDB</a></ListItem>
+                            <ListItem><a href='https://railway.org'>Railway</a></ListItem>
+                        </List>
+                    </Box>
+                  </GridItem>
+                </Grid>
+            </Box>    
+        </Flex>
     </Container>
     )
 }
