008d5f403cf83bbabaaead94d295c6cdcebfe0a2
Jerome A <jeromealapad13@gmail.com>
2023-04-27 13:43:31 +0800

    added section animation

--- pages/works.tsx
+++ pages/works.tsx
@@ -1,5 +1,6 @@
 import { Box, Container, Flex, Grid, GridItem, Image, Heading, Text, useColorModeValue, List, ListItem } from '@chakra-ui/react'
 import Head from 'next/head'
+import Section from 'components/section'
 
 const Works = () => {
     return(
@@ -14,37 +15,39 @@ const Works = () => {
         <Heading variant='works-item'>
             &lt; L I B R A R Y &gt; EBOOK-REPOSITORY
         </Heading>
-        <Flex justifyContent='center'>
-            <Box my={1} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>
-                <Image borderRadius='lg'
-                       alt='library repo'
-                       src='/images/library.png'
-                        />
-                 <Grid templateColumns='repeat(2, 1fr)'>
-                  <GridItem>
-                    <Box mx={10} p={2} textAlign='center'>
-                        <Text variant='work-lang'>LANG used</Text>
-                        <List>
-                            <ListItem><a href='https://python.org'>Python</a></ListItem>
-                            <ListItem><a href='https://javascript.com'>Javascript</a></ListItem>
-                        </List>
-                    </Box>    
-                  </GridItem>
-                  <GridItem>
-                    <Box mx={10} p={2} textAlign='center'>
-                        <Text variant='work-stack'>TECH used</Text>
-                        <List>
-                            <ListItem><a href='https://react.dev'>React</a></ListItem>
-                            <ListItem><a href='https://nodejs.org'>Node.js</a></ListItem>
-                            <ListItem><a href='https://fastapi.tiangolo.com'>FastAPI</a></ListItem>
-                            <ListItem><a href='https://mongodb.com'>MongoDB</a></ListItem>
-                            <ListItem><a href='https://railway.org'>Railway</a></ListItem>
-                        </List>
-                    </Box>
-                  </GridItem>
-                </Grid>
-            </Box>    
-        </Flex>
+        <Section>
+            <Flex justifyContent='center'>
+                <Box my={1} borderRadius='lg' bg={useColorModeValue('cyan.100', 'blackAlpha.300')}>
+                    <Image borderRadius='lg'
+                           alt='library repo'
+                           src='/images/library.png'
+                            />
+                     <Grid templateColumns='repeat(2, 1fr)'>
+                      <GridItem>
+                        <Box mx={10} p={2} textAlign='center'>
+                            <Text variant='work-lang'>LANG used</Text>
+                            <List>
+                                <ListItem><a href='https://python.org'>Python</a></ListItem>
+                                <ListItem><a href='https://javascript.com'>Javascript</a></ListItem>
+                            </List>
+                        </Box>    
+                      </GridItem>
+                      <GridItem>
+                        <Box mx={10} p={2} textAlign='center'>
+                            <Text variant='work-stack'>TECH used</Text>
+                            <List>
+                                <ListItem><a href='https://react.dev'>React</a></ListItem>
+                                <ListItem><a href='https://nodejs.org'>Node.js</a></ListItem>
+                                <ListItem><a href='https://fastapi.tiangolo.com'>FastAPI</a></ListItem>
+                                <ListItem><a href='https://mongodb.com'>MongoDB</a></ListItem>
+                                <ListItem><a href='https://railway.org'>Railway</a></ListItem>
+                            </List>
+                        </Box>
+                      </GridItem>
+                    </Grid>
+                </Box>    
+            </Flex>
+        </Section>
     </Container>
     )
 }
