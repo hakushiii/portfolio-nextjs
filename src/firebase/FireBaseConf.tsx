ec7f34ee6c9ec38d4a27f33cd51e56cedd3bf6c6
Jerome A <jeromealapad13@gmail.com>
2023-05-25 14:31:09 +0800

    firebase config

--- src/firebase/FireBaseConf.tsx
+++ src/firebase/FireBaseConf.tsx
@@ -3,13 +3,14 @@ import { getDatabase } from 'firebase/database'
 
 const startFirebaseDB = () => {
     const config = {
-        apiKey: "AIzaSyAxc8zhc-s5ASLgh7nzmU7b6_t3yii3BaE",
-        authDomain: "dynazenon-hksh.firebaseapp.com",
-        projectId: "dynazenon-hksh",
-        storageBucket: "dynazenon-hksh.appspot.com",
-        messagingSenderId: "542379814829",
-        appId: "1:542379814829:web:91dda779a3fb5b8d873b24",
-        measurementId: "G-8NB0SE92PL"
+        apiKey: "AIzaSyD3Q6n6FW-hQM_NaHVgop5uWAP4aU0_fuI",
+        authDomain: "dynazenon-ada3d.firebaseapp.com",
+        databaseURL: "https://dynazenon-ada3d-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "dynazenon-ada3d",
+        storageBucket: "dynazenon-ada3d.appspot.com",
+        messagingSenderId: "796345421239",
+        appId: "1:796345421239:web:397840d80ca26743056c32",
+        measurementId: "G-GTCKXLR0R7"
     }
 
     const app = initializeApp(config)
